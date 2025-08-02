831b1a3de030a8c4bcc52740d95f53b8829a6b00
Mainca123 <giangchinhtgp@gmail.com>
2025-08-02 16:42:03 +0700

    add front project

---
+++ src/main/java/com/example/HeThongQuanLyTaiChinhThongMinh/HeThongQuanLyTaiChinhThongMinhApplication.java
@@ -0,0 +1,13 @@
+package com.example.HeThongQuanLyTaiChinhThongMinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeThongQuanLyTaiChinhThongMinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeThongQuanLyTaiChinhThongMinhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HeThongQuanLyTaiChinhThongMinh/HeThongQuanLyTaiChinhThongMinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HeThongQuanLyTaiChinhThongMinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeThongQuanLyTaiChinhThongMinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
